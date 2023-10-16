05f20c4cd8b7732cd57865a620e7a41cc2ec353e
Gustavo Hildebrand <gustavo_hbatista@yahoo.com.br>
2023-10-15 23:38:35 -0300

    config

--- src/page/Cakes/Cakes.jsx
+++ src/page/Cakes/Cakes.jsx
@@ -36,7 +36,7 @@ export default function Cakes() {
     }
 
     const onSubmit = (data) => {
-        const cake = { id: `${data.recheio}${data.cobertura}${data.tamanho}${data.sabor}`, 
+        const cake = { id: 1, 
         name: "Bolo Personalizado", description: `Bolo personalizado de ${data.recheio} com cobertura de ${data.cobertura} sabor ${data.sabor}`, amount: 30, quantity: 1 };
         setOpenModal(false);
         addCakeAction(cake);
